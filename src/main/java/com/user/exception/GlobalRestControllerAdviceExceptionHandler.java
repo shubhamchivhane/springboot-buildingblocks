0fefc4343df177ed50cca0e95dd6b92a171251c6
shubhamchivhane <shubhamkc@yahoo.com>
2021-03-21 11:57:42 +0530

    testing 123

--- src/main/java/com/user/exception/GlobalRestControllerAdviceExceptionHandler.java
+++ src/main/java/com/user/exception/GlobalRestControllerAdviceExceptionHandler.java
@@ -1,8 +1,19 @@
 package com.user.exception;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalRestControllerAdviceExceptionHandler {
 
+	@ExceptionHandler(UserNameNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public CustomeErrorDetails customErrorDetails(UserNameNotFoundException e)
+	{
+		return new CustomeErrorDetails(new Date(),"Not Found Exception rest",e.getMessage());
+	}
 }
